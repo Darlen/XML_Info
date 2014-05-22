7463557dd56fda47cf692e4eee3014ff3a89f116
darlen <tree3170@gmail.com>
2014-05-22 14:31:00 +0800

    add some file

---
+++ XML_Validation/src/com/darlen/xml/parse/XMLParse.java
@@ -0,0 +1,11 @@
+package com.darlen.xml.parse;
+
+/**
+ * 这个类主要用于XML的解析问题
+ * Created with IntelliJ IDEA.
+ * User: darlenliu
+ * Date: 14-5-22
+ * Time: 下午2:18
+ */
+public class XMLParse {
+}
---
+++ XML_Validation/src/com/darlen/xml/transfer/XMLTransfer.java
@@ -0,0 +1,11 @@
+package com.darlen.xml.transfer;
+
+/**
+ * 这个类主要用于XML与JSON/javabean之间的转换
+ * Created with IntelliJ IDEA.
+ * User: darlenliu
+ * Date: 14-5-22
+ * Time: 下午2:20
+ */
+public class XMLTransfer {
+}
